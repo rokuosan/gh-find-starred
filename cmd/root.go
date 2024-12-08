29555805bb5b60acc4b46c3b0a3f8d6bdb9d4c75
Koki Matsumoto <85651386+rokuosan@users.noreply.github.com>
2024-12-08 22:14:56 +0900

    wip

--- cmd/root.go
+++ cmd/root.go
@@ -1,11 +1,16 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
+	"time"
 
-	"github.com/cli/go-gh/v2/pkg/api"
+	"github.com/charmbracelet/bubbles/spinner"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/cli/go-gh/v2/pkg/config"
-	graphql "github.com/cli/shurcooL-graphql"
+	"github.com/rokuosan/gh-find-starred/internal/github"
 	"github.com/spf13/cobra"
 )
 
@@ -14,11 +19,11 @@ var rootCmd = &cobra.Command{
 	Short: "gh-find-starred is a GitHub CLI extension to find your starred repositories.",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Cache: " + config.CacheDir())
-		fmt.Println("Config: " + config.ConfigDir())
-
-		repo := GetStarredRepositories("")
-		fmt.Printf("Got: %d\n", len(repo))
+		p := tea.NewProgram(initialModel())
+		if _, err := p.Run(); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
 	},
 }
 
@@ -28,74 +33,161 @@ func Execute() {
 
 func init() {}
 
-type Repository struct {
-	Name   string `json:"name"`
-	Url    string `json:"url"`
-	Readme string `json:"readme"`
+type RepositoryListItem github.Repository
+
+func (i RepositoryListItem) Title() string       { return i.Name }
+func (i RepositoryListItem) FilterValue() string { return i.Name }
+
+type errMsg error
+
+type FetchRepositoryMessage github.GetStarredRepositoriesResult
+
+type model struct {
+	spinner  spinner.Model
+	quitting bool
+	err      error
+	loading  bool
+	repos    []github.Repository
+	cursor   string
 }
 
-func GetStarredRepositories(after string) []Repository {
-	c, err := api.DefaultGraphQLClient()
-	if err != nil {
-		panic(err)
+func initialModel() model {
+	// スピナーを初期化
+	s := spinner.New()
+	s.Spinner = spinner.Dot
+	s.Style = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
+
+	// モデルを初期化
+	return model{
+		spinner: s,
+		loading: true,
 	}
+}
 
-	// Query
-	var query struct {
-		Viewer struct {
-			StarredRepositories struct {
-				Nodes []struct {
-					Name string
-					Url  string
+func (m model) Init() tea.Cmd {
+	return tea.Batch(
+		m.spinner.Tick,
+		m.GetRepositoriesFromGitHub,
+	)
+}
 
-					Object struct {
-						Blob struct {
-							Text string
-						} `graphql:"... on Blob"`
-					} `graphql:"object(expression: $expression)"`
+func (m model) GetRepositoriesFromGitHub() tea.Msg {
+	// キャッシュを取得
+	dir := config.CacheDir()
+	path := fmt.Sprintf("%s/starred_repositories.json", dir)
+	if f, err := os.Open(path); err == nil {
+		defer f.Close()
+		var cacheData struct {
+			ExpiresAt string              `json:"expires_at"`
+			CreatedAt string              `json:"created_at"`
+			Data      []github.Repository `json:"data"`
+		}
+		if err := json.NewDecoder(f).Decode(&cacheData); err == nil {
+			// キャッシュが有効であればリポジトリを返す
+			if expiresAt, err := time.Parse(time.RFC3339, cacheData.ExpiresAt); err == nil {
+				if time.Now().Before(expiresAt) {
+					repos := make([]github.Repository, len(cacheData.Data))
+					for i, d := range cacheData.Data {
+						repos[i] = d
+					}
+					return FetchRepositoryMessage(github.GetStarredRepositoriesResult{
+						Repositories: repos,
+						PageInfo:     github.PageInfo{HasNextPage: false},
+					})
 				}
-				PageInfo struct {
-					HasNextPage bool
-					EndCursor   string
-				}
-			} `graphql:"starredRepositories(after: $after, first: 100)"`
-		} `graphql:"viewer"`
-		RateLimit struct {
-			Cost      int    `graphql:"cost"`
-			Limit     int    `graphql:"limit"`
-			Remaining int    `graphql:"remaining"`
-			ResetAt   string `graphql:"resetAt"`
-		} `graphql:"rateLimit"`
-	}
-
-	// Variables
-	variables := map[string]interface{}{
-		"expression": graphql.String("HEAD:README.md"),
-		"after":      graphql.String(after),
-	}
-
-	// Execute
-	err = c.Query("repositories", &query, variables)
-	if err != nil {
-		panic(err)
-	}
-
-	repos := make([]Repository, len(query.Viewer.StarredRepositories.Nodes))
-	// Shwo rate limit
-	fmt.Printf("Rate limit: %d/%d\n", query.RateLimit.Remaining, query.RateLimit.Limit)
-	if !query.Viewer.StarredRepositories.PageInfo.HasNextPage {
-		// Return repositories if there are no more pages
-		for i, node := range query.Viewer.StarredRepositories.Nodes {
-			repos[i] = Repository{
-				Name:   node.Name,
-				Url:    node.Url,
-				Readme: node.Object.Blob.Text,
 			}
 		}
-		return repos
-	} else {
-		// Recursively get more repositories
-		after = query.Viewer.StarredRepositories.PageInfo.EndCursor
-		return append(repos, GetStarredRepositories(after)...)
 	}
+
+	// 現在のカーソルからリポジトリを取得する
+	result, err := github.GetStarredRepositories(m.cursor)
+	if err != nil {
+		return errMsg(err)
+	}
+	// 取得したリポジトリを返す
+	return FetchRepositoryMessage(result)
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyEscape, tea.KeyCtrlC:
+			m.quitting = true
+			return m, tea.Quit
+		}
+
+	case FetchRepositoryMessage:
+		var cmd tea.Cmd
+		m.repos = append(m.repos, msg.Repositories...)
+		m.cursor = msg.PageInfo.EndCursor
+
+		if msg.PageInfo.HasNextPage {
+			cmd = m.GetRepositoriesFromGitHub
+		}
+
+		return m, cmd
+
+	case errMsg:
+		m.err = msg
+		return m, nil
+
+	case spinner.TickMsg:
+		var cmd tea.Cmd
+		m.spinner, cmd = m.spinner.Update(msg)
+		return m, cmd
+
+	}
+
+	return m, nil
+}
+
+func (m model) View() string {
+	if m.err != nil {
+		return m.err.Error()
+	}
+	if m.quitting {
+		return "\n"
+	}
+	if m.loading {
+		count := len(m.repos)
+		str := fmt.Sprintf("%s Collecting your starred repositories: %d\n", m.spinner.View(), count)
+		return str
+	}
+
+	return ""
+}
+
+type CacheData struct {
+	ExpiresAt string        `json:"expires_at"`
+	CreatedAt string        `json:"created_at"`
+	Data      []interface{} `json:"data"`
+}
+
+func Cache(path string, data []github.Repository) error {
+	now := time.Now().Format(time.RFC3339)
+	expiresAt := time.Now().Add(time.Hour * 24).Format(time.RFC3339)
+	cacheData := CacheData{
+		ExpiresAt: expiresAt,
+		CreatedAt: now,
+		Data:      make([]interface{}, len(data)),
+	}
+	for i, d := range data {
+		cacheData.Data[i] = d
+	}
+
+	// ファイルを作成
+	f, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	// ファイルに書き込む
+	enc := json.NewEncoder(f)
+	if err := enc.Encode(cacheData); err != nil {
+		return err
+	}
+
+	return nil
 }
---
+++ internal/github/repository.go
@@ -0,0 +1,105 @@
+package github
+
+import (
+	"github.com/cli/go-gh/v2/pkg/api"
+	gql "github.com/cli/shurcooL-graphql"
+)
+
+type Repository struct {
+	Name        string `json:"name"`
+	Url         string `json:"url"`
+	Description string `json:"description"`
+	Readme      string `json:"readme"`
+}
+
+type GetStarredRepositoriesQuery struct {
+	Viewer struct {
+		StarredRepositories struct {
+			Nodes []struct {
+				Name        string
+				Url         string
+				Description string
+
+				Object struct {
+					Blob struct {
+						Text string
+					} `graphql:"... on Blob"`
+				} `graphql:"object(expression: $expression)"`
+			}
+			PageInfo PageInfo `graphql:"pageInfo"`
+		} `graphql:"starredRepositories(after: $after, first: 100)"`
+	} `graphql:"viewer"`
+	RateLimit RateLimit `graphql:"rateLimit"`
+}
+
+func (q GetStarredRepositoriesQuery) Repositories() []Repository {
+	repos := make([]Repository, len(q.Viewer.StarredRepositories.Nodes))
+	for i, node := range q.Viewer.StarredRepositories.Nodes {
+		repos[i] = Repository{
+			Name:        node.Name,
+			Url:         node.Url,
+			Description: node.Description,
+			Readme:      node.Object.Blob.Text,
+		}
+	}
+	return repos
+}
+
+func (q GetStarredRepositoriesQuery) PageInfo() PageInfo {
+	return q.Viewer.StarredRepositories.PageInfo
+}
+
+type GetStarredRepositoriesResult struct {
+	Repositories []Repository
+	PageInfo     PageInfo
+}
+
+func GetStarredRepositories(after string) (GetStarredRepositoriesResult, error) {
+	c, err := api.DefaultGraphQLClient()
+	if err != nil {
+		return GetStarredRepositoriesResult{}, err
+	}
+
+	var query GetStarredRepositoriesQuery
+	err = c.Query("StarredRepositories", &query, map[string]interface{}{
+		"expression": gql.String("HEAD:README.md"),
+		"after":      gql.String(after),
+	})
+
+	if err != nil {
+		return GetStarredRepositoriesResult{}, err
+	}
+
+	return GetStarredRepositoriesResult{
+		Repositories: query.Repositories(),
+		PageInfo:     query.PageInfo(),
+	}, nil
+}
+
+type Repositories []Repository
+
+type SearchRepositoryOptions struct {
+	IncludeDescription bool
+	IncludeREADME      bool
+}
+
+type SearchRepositoryResult []SearchRepositoryResultItem
+
+type SearchRepositoryResultItem struct {
+	Repository Repository
+	Point      int
+}
+
+func (r Repositories) Search(query string, opt *SearchRepositoryOptions) SearchRepositoryResult {
+	var results SearchRepositoryResult
+	for _, repo := range r {
+		if repo.Name == query {
+			r := SearchRepositoryResultItem{
+				Repository: repo,
+				Point:      100,
+			}
+			results = append(results, r)
+		}
+	}
+	return results
+}
